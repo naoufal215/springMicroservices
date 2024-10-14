2e7b720863c26a6bf4b0ecfde1bb31f2f25fa88a
naoufal berouain <naoufal.berouain@gmail.com>
2024-10-14 17:37:26 +0200

    imporving bash script to test MS

--- test-em-all.bash
+++ test-em-all.bash
@@ -3,7 +3,7 @@
 #
 #
 : ${HOST=localhost}
-: ${PORT=7000}
+: ${PORT=8080}
 : ${PROD_ID_REVS=1}
 : ${PROD_ID_NOT_FOUND=12}
 : ${PROD_ID_NO_REVS=213}
@@ -34,15 +34,47 @@ function assertCurl(){
 function assertEqual() {
 	local expected=$1
 	local actual=$2
+	local name=$3
 	
 	if [	"$actual" = "$expected"	]
 	then
-		echo "Test OK (actual value: $actual)"
+		echo "Test of ($name) OK (actual value: $actual)"
 	else
-		echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
+		echo "Test of ($name) FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
 		exit 1
 	fi
 
+}
+
+function testUrl(){
+	
+	url=$@
+	
+	if $url -ks -f -o /dev/null
+	then
+		return 0
+	else
+		return 1
+	fi;
+}
+
+function waitForService(){
+	url=$@
+	echo -n "wait for: $url..."
+	n=0
+	until testUrl $url
+	do
+		n=$((n+1))
+		if [[ $n == 100 ]]
+		then
+			echo " Give up"
+			exit 1
+		else
+			sleep 3
+			echo -n ", retry #$n"
+		fi
+	done
+	echo "Done, continues..."
 }
 
 set -e
@@ -50,23 +82,57 @@ set -e
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
+if [[ $@ == *"start"* ]]
+then
+	echo "Restarting the test rnvironment"
+	echo "$ docker compose down --remove-orphans"
+	docker compose down --remove-orphans
+	echo "$ docker compose up -d"
+	docker compose up -d
+fi
 
-assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS -s"
-assertEqual $PROD_ID_REVS $(echo $RESPONSE | jq .productId)
-assertEqual 4 $(echo $RESPONSE | jq ".reviews | length")
+waitForService curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS
+echo -e "\n"
 
-assertCurl 404 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s"
-assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPONSE | jq -r .message)"
 
-assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_REVS -s"
-assertEqual $PROD_ID_NO_REVS $(echo $RESPONSE | jq .productId)
-assertEqual 0 $(echo $RESPONSE | jq ".reviews | length")
+echo "Start of test: get product with reviews using productId"
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS -s" "response status"
+assertEqual $PROD_ID_REVS $(echo $RESPONSE | jq .productId) "product id"
+assertEqual 4 $(echo $RESPONSE | jq ".reviews | length") "reviews length"
+echo "End of test: get product with reviews using productId" 
+echo -e "\n"
 
-assertCurl 422 "curl http://$HOST:$PORT/product-composite/-1 -s"
-assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
+echo "Start of test: get product with using unkown productId"
+assertCurl 404 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s" "response status"
+assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPONSE | jq -r .message)" "response content"
+echo "End of test: get product with using unkown productId"
+echo -e "\n"
 
-assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s"
-assertEqual null "$(echo $RESPONSE | jq -r .message)"
+echo "Start of test: get product without reviews using productId"
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_REVS -s" "response status"
+assertEqual $PROD_ID_NO_REVS $(echo $RESPONSE | jq .productId) "product id"
+assertEqual 0 $(echo $RESPONSE | jq ".reviews | length") "reviews length"
+echo "End of test: get product without reviews using productId"
+echo -e "\n"
+
+echo "Start of test: get product using negative productId"
+assertCurl 422 "curl http://$HOST:$PORT/product-composite/-1 -s" "response status"
+assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)" "response content"
+echo "End of test: get product using negative productId" 
+echo -e "\n"
+
+echo "Start of test: get product using string as productId"
+assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s" "response status"
+assertEqual null "$(echo $RESPONSE | jq -r .message)" "response content"
+echo "End of test: get product using string as productId" 
+echo -e "\n"
+
+if [[ $@ == *"stop"* ]]
+then
+	echo "We are done, stopping the test environment..."
+	echo "$ docker compose down"
+	docker compose down 
+fi
 
 echo "END, all test OK:" `date`
 
